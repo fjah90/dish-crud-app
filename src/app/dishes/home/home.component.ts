a740ecee64e40bb1ee194ced2981e60268fecd39
Fernando Aponte <fernandoaponte90@gmail.com>
2023-02-06 18:35:00 -0400

    fix: add s to scss

--- src/app/dishes/home/home.component.ts
+++ src/app/dishes/home/home.component.ts
@@ -5,7 +5,7 @@ import { DishesService } from '../dishes.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   allDishes: Dishes[] = [];
